65da1c56045214c04beb2c06e04f797755b7f83c
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-15 02:01:43 +0900

    フォロー対象とフォロワーの参照を追加、マイグレーション実行

--- db/migrate/20250414165830_create_relationships.rb
+++ db/migrate/20250414165830_create_relationships.rb
@@ -1,7 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[8.0]
   def change
-    create_table :relationships do |t|
-      t.timestamps
+    create_table :relationship do |r|
+      r.references :followed, foreign_key: { to_table: :users }
+      r.references :follower, foreign_key: { to_table: :users }
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_14_153427) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_14_165830) do
   create_table "posts", force: :cascade do |t|
     t.string "content", limit: 140
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationship", force: :cascade do |t|
+    t.integer "followed_id"
+    t.integer "follower_id"
+    t.index ["followed_id"], name: "index_relationship_on_followed_id"
+    t.index ["follower_id"], name: "index_relationship_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +37,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_14_153427) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "relationship", "users", column: "followed_id"
+  add_foreign_key "relationship", "users", column: "follower_id"
 end
