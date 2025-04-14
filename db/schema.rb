966658092282e9fb43ab604aaaa96c6d27404200
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-15 00:44:05 +0900

    AddUserrefのマイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_14_141728) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_14_153427) do
   create_table "posts", force: :cascade do |t|
     t.string "content", limit: 140
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
