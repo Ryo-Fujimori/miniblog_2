82782b69760bd39d16d3f58b1045910f8a32ee5e
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-15 02:02:58 +0900

    Userモデルに関連付け追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :active_relationship, class_name: :user, foreign_key: :followed
+  has_many :active_relationship, class_name: :user, foreign_key: :follower
 end
