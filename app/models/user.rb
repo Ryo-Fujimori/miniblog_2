7bc4b890b8f835d6dbb29f36c7bc25d2ea00fe14
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-15 19:06:28 +0900

    Userモデルにフォローアクション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :active_relationship, class_name: :user, foreign_key: :followed
   has_many :active_relationship, class_name: :user, foreign_key: :follower
+
+  def follow(user_id)
+    user = User.find(user_id)
+    current_user.active_relationship.build(user)
+  end
 end
