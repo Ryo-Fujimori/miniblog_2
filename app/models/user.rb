263c91a7e3484d6ba1485d05a498d635e2988e92
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-19 17:03:52 +0900

    relationshipとuserの関連を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :active_relationship, class_name: :user, foreign_key: :followed
-  has_many :active_relationship, class_name: :user, foreign_key: :follower
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
 
   def follow(user_id)
     user = User.find(user_id)
     current_user.active_relationship.create(user)
+  end
+
+  def unfollow
+    Relationship.find(params[:id]).destroy!
   end
 end
