af77f8b7969b904dda53a6b53b28aeee8cb1e16b
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-19 18:20:58 +0900

    relationshipコントローラー、create、destroyアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "posts#index"
   resources :posts
+  resources :relationships, only: [ :create, :destroy ]
 end
