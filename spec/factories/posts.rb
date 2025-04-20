6ef89cb83a220fcfcaf98da6202cc8cbdb149762
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-20 23:13:58 +0900

    factory モデル作成
    rails g factory_bot:model user create spec/factories/users.rb
    rails g factory_bot:model post create spec/factories/posts.rb

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post do
+    create { "MyString" }
+    spec/factories/posts.rb { "MyString" }
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    create { "MyString" }
+    spec/factories/users.rb { "MyString" }
+  end
+end
