1846db8deecc3d58a6ce8346aee41f20a95b4708
Hiroshi Ueki <ueki@sonicgarden.jp>
2025-04-21 20:26:33 +0900

    comment

--- db/migrate/20250414165830_create_relationships.rb
+++ db/migrate/20250414165830_create_relationships.rb
@@ -1,8 +1,12 @@
 class CreateRelationships < ActiveRecord::Migration[8.0]
   def change
     create_table :relationships do |r|
-      r.references :followed, foreign_key: { to_table: :users }
+      # ユニークインデックスと重複しているので、index: falseにする
+      r.references :followed, foreign_key: { to_table: :users }, index: false
       r.references :follower, foreign_key: { to_table: :users }
     end
+
+    # ユニークインデックスを追加
+    add_index :relationships, [:followed_id, :follower_id], unique: true
   end
 end
