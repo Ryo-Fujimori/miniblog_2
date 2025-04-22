466bae5d99fb1e2483d79d74205c3a0a73bbb496
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-23 00:13:47 +0900

    コミット

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,13 @@ module Miniblog2
   class Application < Rails::Application
     config.load_defaults 8.0
     config.autoload_lib(ignore: %w[assets tasks])
-
-    
   end
+end
+
+config.generators do |g|
+  g.helper false
+  g.test_framework :rspec,
+                   routing_specs: false,
+                   request_specs: false,
+                   view_specs: false
 end
